7fc5d5f328fcc760c99f5fb98026f17c841f511b
Kagami <xuxiaojie42@gmail.com>
2015-08-26 16:59:24 +0800

    implement

--- Averagegridlayout/sample/src/main/java/com/kagami/averagegridlayout/sample/MainActivity.java
+++ Averagegridlayout/sample/src/main/java/com/kagami/averagegridlayout/sample/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.kagami.averagegridlayout.AverageGridLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final AverageGridLayout averageGridLayout=(AverageGridLayout)findViewById(R.id.view);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
     @Override
